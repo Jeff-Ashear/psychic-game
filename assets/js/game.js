d9b12e86e9efa8862a48f681b6b7f20b26f48eaa
Jeff.A <kinesthesiac@gmail.com>
2021-05-16 18:00:41 -0700

    files and psuedocode

---
+++ assets/js/game.js
@@ -0,0 +1,24 @@
+// array stores possible letters
+// wins var
+//losses var
+
+// function game start
+    // generates a random number between 0 and 25 and stores in variable
+        // this number references the index within the array
+    //sets the number of guesses left to 9
+
+    
+
+//function user input
+    //listens for user input
+        //filters for letters only
+    //if guesses left >0 compares user input to the letter at index of possible letters array
+        // if user input matches letter
+            //wins +1
+            //table resets
+            //new game starts
+        //else
+            //number of guesses -1
+    //else
+        //losses +1
+        //table resets
